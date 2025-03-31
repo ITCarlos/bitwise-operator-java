453c03cc9b3f19cdbe7737eff23caca28a2c8bc1
unknown <carlosafdev@gmail.com>
2025-03-31 18:52:58 -0300

    Repository created

---
+++ src/application/Program.java
@@ -0,0 +1,15 @@
+package application;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		int n1 = 89;
+		int n2 = 60;
+		System.out.println(n1 & n2); //function "and"
+		System.out.println(n1 | n2); //function "or"
+		System.out.println(n1 ^ n2); //function "exclusive-nor"
+
+	}
+
+}
